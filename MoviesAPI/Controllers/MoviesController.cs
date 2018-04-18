8ec4d0141af1334361e7dc010329fe5eeb2a3068
Rigney <krigney@proforma.com>
2018-04-18 13:24:44 -0400

    Got API Get working with a simple string.

--- MoviesAPI/Controllers/MoviesController.cs
+++ MoviesAPI/Controllers/MoviesController.cs
@@ -1,17 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MoviesAPI.Data;
+using MoviesAPI.Models;
 
 namespace MoviesAPI.Controllers
 {
-    public class MoviesController : Controller
+    public class MoviesController : ApiController
     {
-        // GET: Movies
-        public ActionResult Index()
+        public IEnumerable<Movie> Get()
         {
-            return View();
+            var db = new DataContext();
+            return db.Movies.ToList();
         }
+
     }
 }
