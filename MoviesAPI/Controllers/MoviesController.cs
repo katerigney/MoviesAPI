1e00adc1c0da077e79602250a485081cdfd8fb3e
Rigney <krigney@proforma.com>
2018-04-18 14:00:40 -0400

    Added post movie and search by genre.

--- MoviesAPI/Controllers/MoviesController.cs
+++ MoviesAPI/Controllers/MoviesController.cs
@@ -11,11 +11,29 @@ namespace MoviesAPI.Controllers
 {
     public class MoviesController : ApiController
     {
-        public IEnumerable<Movie> Get()
+        public IEnumerable<Movie> GetAllMovies()
         {
             var db = new DataContext();
-            return db.Movies.ToList();
+            return db.Movies.ToList(); 
         }
 
+        public Movie Post(Movie newMovie)
+        {
+            var db = new DataContext();
+            db.Movies.Add(newMovie);
+            db.SaveChanges();
+            return newMovie;
+        }
+
+        public IEnumerable<Movie> GetMovieGenre(string genre)
+        {
+            var db = new DataContext();
+            var horrorMovies = db.Movies.Where(movie => movie.Genre == genre);
+            return horrorMovies;
+        }
     }
 }
+
+//Create GET /Search? title = Jurassic that returns all movies where the title contains the title parameter
+
+//Create a GET /Movies? genre = { Genre } that returns movies of only that Genre
